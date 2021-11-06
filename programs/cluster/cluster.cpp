118669d34519db89f8e9ea91c7f661bf6a8b98a2
EliasEconomou <eliasgybe@hotmail.com>
2021-11-06 12:16:36 +0200

    cluster prepare

---
+++ programs/cluster/cluster.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+
+// kalytera na kaneis parse ta arguments ths main me allon tropo ap oti exw kanei sta lsh/cube, giati edw exei kai to 'complete' pou 
+// einai optional argument
+
+
+
+
+
+
+
+
+
+}
